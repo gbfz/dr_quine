abaa1e4b18ef913c372f19fc363edb1e39d16775
gbfz <gbfiglin@gmail.com>
2022-08-02 01:38:44 +0300

    Grace...... That was frustrating

---
+++ Grace/Grace.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#define WRAP(s) QUOTE(s)
+#define QUOTE(s) #s
+#define EXEC(x) int main() { FILE* kid = fopen("./Grace_kid.c", "w"); fputs("#include <stdio.h>\n\n#define WRAP(s) QUOTE(s)\n#define QUOTE(s) #s\n#define EXEC(x) "x"\n\nEXEC(WRAP(EXEC(x)))", kid); fputc(10, kid); fclose(kid); }
+
+EXEC(WRAP(EXEC(x)))
