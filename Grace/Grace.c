691257dd77b1b4901e4debf5514dc2df76d59c2c
gbfz <gbfiglin@gmail.com>
2022-08-02 01:53:38 +0300

    Changed a char in Grace

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -2,6 +2,6 @@
 
 #define WRAP(s) QUOTE(s)
 #define QUOTE(s) #s
-#define EXEC(x) int main() { FILE* kid = fopen("./Grace_kid.c", "w"); fputs("#include <stdio.h>\n\n#define WRAP(s) QUOTE(s)\n#define QUOTE(s) #s\n#define EXEC(x) "x"\n\nEXEC(WRAP(EXEC(x)))", kid); fputc(10, kid); fclose(kid); }
+#define EXEC(x) int main() { FILE* kid = fopen("./Grace_kid.c", "w"); fputs("#include <stdio.h>\n\n#define WRAP(s) QUOTE(s)\n#define QUOTE(s) #s\n#define EXEC(x) "x"\n\nEXEC(WRAP(EXEC(x)))", kid); fputc('\n', kid); fclose(kid); }
 
 EXEC(WRAP(EXEC(x)))
