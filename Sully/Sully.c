578fd2865cb0c550a6b61a7892ac390d46e51737
gbfz <gbfiglin@gmail.com>
2022-08-03 20:30:16 +0300

    That's the version I prefer, so I'll save it to git, but there are constraints that I didn't follow in this version

--- Sully/Sully.c
+++ Sully/Sully.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <sys/stat.h>
 
-char* strdup(const char* s);
+char* strdup(const char*);
 
 char* concat(const char* a, const char* b)
 {
@@ -15,26 +15,26 @@ char* concat(const char* a, const char* b)
 
 int createFile(const char* filename)
 {
-	FILE* file = fopen(filename, "w");
+	FILE* kid = fopen(filename, "w");
 	FILE* this = fopen(__FILE__, "r");
-	if (!file || !this)
-		return 0;
+	if (!kid || !this)
+		return 0xBADF1L;
 	struct stat st;
 	stat(__FILE__, &st);
 	char* buf = malloc(st.st_size);
 	if (!buf)
-		return 0;
+		return 0xBADALL;
 	fread(buf, st.st_size, 1, this);
 	fclose(this);
-	fwrite(buf, st.st_size, 1, file);
-	fclose(file);
+	fwrite(buf, st.st_size, 1, kid);
+	fclose(kid);
 	free(buf);
-	return 1;
+	return 0;
 }
 
-int exec(const char* filename)
+int launch(const char* filename)
 {
-	if (!createFile(filename))
+	if (createFile(filename) != 0)
 		return 0xDEAD;
 	char* cmd0 = concat("clang ", filename);
 	char* cmd = concat(cmd0, " -o Sully; ./Sully");
@@ -47,19 +47,21 @@ int exec(const char* filename)
 int main()
 {
 	int status;
-	if (strcmp(__FILE__, "Sully.c")) {
+	if (strcmp(__FILE__, "Sully.c"))
+	{
 		if (strrchr(__FILE__, '0'))
 			return 0;
 		char *filename = strdup(__FILE__);
 		filename[6] -= 1;
-		status = exec(filename);
+		status = launch(filename);
 		free(filename);
 	}
-	else {
-		char* filename = malloc(strlen(__FILE__) + 3);
+	else
+	{
+		char* filename = calloc(strlen(__FILE__) + 3, 1);
 		strncpy(filename, __FILE__, 5);
 		strcat(filename, "_5.c");
-		status = exec(filename);
+		status = launch(filename);
 		free(filename);
 	}
 	return status;
