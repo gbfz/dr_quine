53be405c410e85aeaf70c4ab7a5918640ea82e19
gbfz <gbfiglin@gmail.com>
2022-08-03 16:02:10 +0300

    Forgot the comment in Grace

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* a comment */
 #define WRAP(s) QUOTE(s)
 #define QUOTE(s) #s
-#define EXEC(x) int main() { FILE* kid = fopen("./Grace_kid.c", "w"); fprintf(kid, "%s\n", "#include <stdio.h>\n\n#define WRAP(s) QUOTE(s)\n#define QUOTE(s) #s\n#define EXEC(x) "x"\n\nEXEC(WRAP(EXEC(x)))"); fclose(kid); }
+#define EXEC(x) int main() { FILE* kid = fopen("./Grace_kid.c", "w"); fprintf(kid, "%s\n", "#include <stdio.h>\n/* a comment */\n#define WRAP(s) QUOTE(s)\n#define QUOTE(s) #s\n#define EXEC(x) "x"\n\nEXEC(WRAP(EXEC(x)))"); fclose(kid); }
 
 EXEC(WRAP(EXEC(x)))
